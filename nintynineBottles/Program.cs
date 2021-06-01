0fb89d602c27d7d46174c4a4efa1ab36e110c297
Abdul-khaaliq Dollie <55828788+Abdulkhaaliq@users.noreply.github.com>
2021-06-01 09:06:58 +0200

    Added

---
+++ nintynineBottles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace nintynineBottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Bottles();
+        }
+
+        public static void Bottles()
+        {
+            for (int i = 99; i >= 1; i--)
+            {
+
+                Console.WriteLine(" '" + i + "' bottles of water on the wall, '" + i + "' bottles of water.");
+                i--; 
+                Console.WriteLine(" Take one done and pass it around " + i + "' bottles of water on the wall.");
+                i++;
+                if (i == 1)
+
+                {
+                    Console.WriteLine(" Thanks for playing but I'm out!!!");
+                }
+            }
+        }
+    }
+}
